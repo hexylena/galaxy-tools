6ad08a0a910f634297264a467c207404441751fe
Eric Rasche <rasche.eric@yandex.ru>
2014-12-06 09:47:31 -0600

    Added docs, removed galaxy.json

--- GSAF/gsaf_download.py
+++ GSAF/gsaf_download.py
@@ -6,7 +6,7 @@ import json
 import hashlib
 import os
 import logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig()
 log = logging.getLogger()
 
 try:
@@ -15,18 +15,20 @@ except:
     #TODO
     pass
 
-URL = sys.argv[1]
-DATASET_ID = sys.argv[2]
+try:
+    URL = sys.argv[1]
+except:
+    log.error("Must provide a url as the only argument")
 
 req = urllib2.urlopen(URL)
 page = req.read()
 comments = [x.strip() for x in page.split('\n') if x.strip().startswith('<!--')]
 
-gx_json = open('galaxy.json', 'w')
-
 prefix = '<!--gsafjson'
 suffix = '-->'
-for gsafjson in [x[len(prefix): -len(suffix)].strip() for x in comments if 'gsafjson' in x]:
+gsafjson_data = [x[len(prefix): -len(suffix)].strip() for x in comments if 'gsafjson' in x]
+print "Found %s files" % len(gsafjson_data)
+for gsafjson in gsafjson_data:
     #{"file_type":"fastq.gz",
     # "JA":"****",
     # "sample_name":"Sample10",
@@ -41,34 +43,13 @@ for gsafjson in [x[len(prefix): -len(suffix)].strip() for x in comments if 'gsaf
     log.info("Fetching %s" % data['filename'])
     file_path = os.path.join('output', data['filename'])
     urllib.urlretrieve(data['url'], file_path)
-    log.info("Hashing file")
     file_md5 = hashlib.md5(open(file_path).read()).hexdigest()
     log.debug("Hashed to %s" % file_md5)
 
-    stderr = ''
     if file_md5 != data['md5']:
-        stderr = 'md5sum mismatch: %s != %s' % (file_md5, data['md5'])
-
-    # Galaxy.json
-    # {"name": "lambda.fa", "stdout": "uploaded fasta file", "line_count": 811, "ext": "fasta", "dataset_id": 16220, "type": "dataset"}
-    line_count = subprocess.check_output(['wc', '-l', file_path]).strip()
-    line_count = line_count[0:line_count.index(' ')]
-
-    galaxy_json = {
-        'name': data['filename'].strip(data['file_type']),
-        'stdout': None,
-        'stderr': stderr,
-        'line_count': int(line_count),
-        # TODO, check that data is really .gz
-        'ext': data['file_type'].strip('.gz'),
-        'dataset_id': DATASET_ID,
-        'type': 'dataset'
-    }
+        log.warning('md5sum mismatch: %s != %s' % (file_md5, data['md5']))
 
     try:
         subprocess.check_call(['gunzip', file_path])
     except:
         log.error("Couldn't extract %s" % data['filename'])
-
-    gx_json.write(json.dumps(galaxy_json) + "\n")
-
